2ecbef96c688d9b60f3e4432a57b3cfb8a725058
neo <sapfioricafe@naver.com>
2021-09-12 13:32:50 +0000

    edit code

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -19,20 +19,20 @@ sap.ui.define([
                         "Addresss": "SAP Fiori Cafe"
                     },
                     "StudyType" : [{
-                        "name": "Fiori",        
+                        "title": "Fiori",        
                         "code": "001",       
                         "desc": "SAP Fiori",
                         "maxnum": "7"
                     },  {
-                        "name": "SAPUI5",        
+                        "title": "SAPUI5",        
                         "code": "002",       
                         "desc": "SAP SAPUI5",
-                        "maxnum": "7"
+                        "maxnum": "5"
                     },  {
-                        "name": "CDS VIEW",        
+                        "title": "CDS VIEW",        
                         "code": "003",       
                         "desc": "SAP CDS VIEW",
-                        "maxnum": "7"
+                        "maxnum": "6"
                         }                                                                                        
                     ]  
                 });
@@ -50,41 +50,41 @@ sap.ui.define([
                                     deleteMode : false
                                 });
 
-                this.setModel(oProPerty, "oProPerty"); //  "oProPerty"  이름으로 Model을  등록 합니다.
+                this.setModel(oProPerty, "oproPerty"); //  "oProPerty"  이름으로 Model을  등록 합니다.
             },
 
             onButtonPress(arg){
-                console.log(arg);
-                var oProPerty = this.getModel("oProPerty");
+
+                sap.m.MessageToast.show(arg);
+
+                var oProPerty = this.getModel("oproPerty");
+
                 this._buttonInit(oProPerty);
 
                 switch(arg){
                     case "display":
-                        oProPerty.setProperty("/readMode", true); 
                         oProPerty.setProperty("/editMode", true); 
-                        oProPerty.setProperty("/saveMode", false); 
-                        oProPerty.setProperty("/deleteMode", false);
                     break;
-                    case "edit":
-                        oProPerty.setProperty("/readMode", false); 
-                        oProPerty.setProperty("/editMode", true); 
+                    case "edit": 
+                        oProPerty.setProperty("/readMode", true); 
                         oProPerty.setProperty("/saveMode", true); 
                         oProPerty.setProperty("/deleteMode", true);
                     break;      
-                                                     
+                    case "save":
+                        oProPerty.setProperty("/readMode", true); 
+                        break;
+                    break;                                      
                 }
-
-
             },
 
             /**
              * 초기화
              */
             _buttonInit : function (oUi) {
-                oUi.setProperty("/readMode", true); // 조회 버튼 클릭시 화면에 노출됩니다. 
-                oUi.setProperty("/editMode", true); // 조회 버튼 클릭시 수정 할수 있도록 기능을 노출 합니다.
-                oUi.setProperty("/saveMode", true); // 조회 버튼 클릭시에는 수정모드가 아니기 때문에 화면에 노출하지 않습니다. 
-                oUi.setProperty("/deleteMode", true);
+                oUi.setProperty("/readMode", false); 
+                oUi.setProperty("/editMode", false); 
+                oUi.setProperty("/saveMode", false); 
+                oUi.setProperty("/deleteMode", false);
             },
 
             getModel : function (sName) {
